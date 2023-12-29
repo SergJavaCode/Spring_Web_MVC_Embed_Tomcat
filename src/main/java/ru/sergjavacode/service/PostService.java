dfcb2e4a4ba66d10e6ae8a13ad4dd38d3e76ca55
Serg <freekey@list.ru>
2023-12-29 15:05:45 +0400

    initial commit

---
+++ src/main/java/ru/sergjavacode/service/PostService.java
@@ -0,0 +1,35 @@
+package ru.sergjavacode.service;
+
+import org.springframework.stereotype.Service;
+import ru.sergjavacode.exception.NotFoundException;
+import ru.sergjavacode.model.Post;
+import ru.sergjavacode.repository.PostRepository;
+
+import java.util.List;
+
+@Service
+public class PostService {
+    private final PostRepository repository;
+
+    public PostService(PostRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Post> all() {
+        return repository.all();
+    }
+
+    public Post getById(long id) {
+        return repository.getById(id).orElseThrow(NotFoundException::new);
+    }
+
+    public Post save(Post post) {
+        post.setRemoved(false);
+        return repository.save(post);
+    }
+
+    public void removeById(long id) {
+        repository.removeById(id);
+    }
+}
+
